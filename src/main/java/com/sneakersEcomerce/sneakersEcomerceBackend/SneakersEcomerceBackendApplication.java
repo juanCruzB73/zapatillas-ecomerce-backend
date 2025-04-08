cfc1f2ed34cc3625aeb277d65e1b9a80640e613b
juanB <juancruzberrios73@gmail.com>
2025-04-08 14:45:08 -0300

    first commit

---
+++ src/main/java/com/sneakersEcomerce/sneakersEcomerceBackend/SneakersEcomerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sneakersEcomerce.sneakersEcomerceBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersEcomerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersEcomerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sneakersEcomerce/sneakersEcomerceBackend/SneakersEcomerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneakersEcomerce.sneakersEcomerceBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersEcomerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
